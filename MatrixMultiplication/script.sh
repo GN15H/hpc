138f64445cfdfb3e16aa4779165e4d2845f80c04
GN15H <cristobalochoacadavid@gmail.com>
2025-09-03 10:33:52 -0500

    pafuncion

---
+++ MatrixMultiplication/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# printf "1, 2, 3, 4 \n" >> a.txt
+# for i in {1..10}; do
+#     for i in 20 40 100 500; do
+    # ./parallelMult.exe $i 1 >> a.txt
+    ./parallelMult.exe 500 1 >> a.txt
+#     done
+# done
